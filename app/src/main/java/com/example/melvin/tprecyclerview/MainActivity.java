ef8829eb44ea98b9143f532641e9bcf818542e90
Melvin Schultz <melvinschultzdev@gmail.com>
2017-05-05 10:32:29 +0200

    add layouts and activities + dependencies

--- app/src/main/java/com/example/melvin/tprecyclerview/MainActivity.java
+++ app/src/main/java/com/example/melvin/tprecyclerview/MainActivity.java
@@ -1,17 +1,38 @@
 package com.example.melvin.tprecyclerview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private Button buttonList;
+    private Button buttonGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        buttonList = (Button) findViewById(R.id.buttonList);
+        buttonGrid = (Button) findViewById(R.id.buttonGrid);
 
+        buttonList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RecyclerListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonGrid.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RecyclerGridActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
