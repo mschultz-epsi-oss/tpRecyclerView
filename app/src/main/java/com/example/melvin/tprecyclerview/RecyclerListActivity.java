c48fb6e1b898e30ccc4391387b6f23794c24f6c6
Melvin Schultz <melvinschultzdev@gmail.com>
2017-05-05 13:53:10 +0200

    add RecyclerView to ListActivity

--- app/src/main/java/com/example/melvin/tprecyclerview/RecyclerListActivity.java
+++ app/src/main/java/com/example/melvin/tprecyclerview/RecyclerListActivity.java
@@ -2,12 +2,17 @@ package com.example.melvin.tprecyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 public class RecyclerListActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_list);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
     }
 }
